be1dcf9baa583e6b4072306ed3289aa8179ce157
nesmamanasra <nesmazak1999@gmail.com>
2022-07-26 14:54:10 +0300

    portfolio-using-bootstrp

---
+++ script.js
@@ -0,0 +1,16 @@
+ // typing text animation script
+ let typed = new Typed(".typing", {
+    strings:[ "Frontend", "Developer" ,"Web" ,"Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+
+let typed1 = new Typed(".typing-2", {
+    strings: [ "Frontend", "Developer" ,"Web" ,"Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
